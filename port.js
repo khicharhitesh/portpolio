66403677f343c740c6f848321885ab2047caf9b3
Your GitHub Username <khicharhitesh065@gmail.com>
2024-12-02 19:16:09 +0530

    upload folder

---
+++ port.js
@@ -0,0 +1,9 @@
+function hamburg() {
+    const dropdown = document.querySelector('.dropdown');
+    dropdown.style.transform = 'translateY(0)';
+}
+
+function cancel() {
+    const dropdown = document.querySelector('.dropdown');
+    dropdown.style.transform = 'translateY(-500px)';
+}
