e4d968f2091e6e80ca16d2dd6bdcd80619e5617e
Your GitHub Username <khicharhitesh065@gmail.com>
2024-12-07 12:23:18 +0530

    chnage

--- port.js
+++
@@ -1,9 +0,0 @@
-function hamburg() {
-    const dropdown = document.querySelector('.dropdown');
-    dropdown.style.transform = 'translateY(0)';
-}
-
-function cancel() {
-    const dropdown = document.querySelector('.dropdown');
-    dropdown.style.transform = 'translateY(-500px)';
-}
